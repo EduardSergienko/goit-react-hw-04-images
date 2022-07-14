42e71930bbdfd83543f57c35047684341be22885
EduardSergienko <katleraster@gmail.com>
2022-07-14 17:45:44 +0300

    Update Notification.jsx

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,5 +1,5 @@
 import styles from './Notification.module.scss';
 
-export function Notification(params) {
+export function Notification() {
   return <h2 className={styles.InfoText}>Please, enter your request</h2>;
 }
